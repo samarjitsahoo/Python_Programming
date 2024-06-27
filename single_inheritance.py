3700b0cb8db936172e219462232d8c42bd35eb5a
Selmon Sahoo <samarjit9203@gmail.com>
2024-06-27 11:35:10 +0530

    Changes

---
+++ single_inheritance.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        
+    def make_sound(self):
+        print("Sound made by the animal")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Dog")
+        self.breed = breed
+        
+    def make_sound(self):
+        print("Bark!")
+
+d = Dog("Dog", "Doggerman")
+d.make_sound()
+
+a = Animal("Dog", "Dog")
+a.make_sound()
+
